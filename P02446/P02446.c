8d57a57c365c70765869a851c3225d54eec2c61d
4hn6 <gb.kim@postech.ac.kr>
2020-07-03 19:18:27 +0900

    P2446 solution source code

---
+++ P02446/P02446.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j;
+
+    scanf("%d", &n);
+
+    for(i = n; i >= 1; i--)
+    {
+        for(j = n; j > i; j--)
+            printf(" ");
+
+        for(j = 1; j <= 2*i - 1; j++)
+            printf("*");
+
+        puts("");
+    }
+
+    for(i = 1; i <= n-1; i++)
+    {
+        for(j = 1; j <= n-1-i; j++)
+            printf(" ");
+
+        for(j = 1; j <= 2*i + 1; j++)
+            printf("*");
+
+        puts("");
+    }
+
+    return 0;
+}
