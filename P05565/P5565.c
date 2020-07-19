fb89d76dd990256f3a46cade73e577a517cbea4d
4hn6 <gb.kim@postech.ac.kr>
2020-07-19 20:19:38 +0900

    P5565 solution source code

---
+++ P05565/P5565.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	int sum, a, b, c, d, e, f, g, h, i, j;
+	scanf("%d", &sum);
+	scanf("%d", &a);
+	scanf("%d", &b);
+	scanf("%d", &c);
+	scanf("%d", &d);
+	scanf("%d", &e);
+	scanf("%d", &f);
+	scanf("%d", &g);
+	scanf("%d", &h);
+	scanf("%d", &i);
+	j=sum-a-b-c-d-e-f-g-h-i;
+	printf("%d", j);
+	return 0;
+}
