5b0b0dde8c13eda4f1c3dfdb3df1c527582e1ef8
4hn6 <gb.kim@postech.ac.kr>
2020-07-21 11:56:27 +0900

    P10872 solution source code

---
+++ P10872/P10872.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int fac(int n)
+{
+    if(n == 0) return 1;
+
+    return n * fac(n-1);
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    printf("%d", fac(n));
+    return 0;
+}
