2cc9203d758985107226be79f02815b85906306e
4hn6 <gb.kim@postech.ac.kr>
2020-07-27 12:12:35 +0900

    P2751 solution source code

---
+++ P02751/P2751.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int num[1000000];
+int main()
+{
+    int n;
+
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++)
+        scanf("%d", num+i);
+
+    sort(num, num+n);
+
+    for(int i = 0; i < n; i++)
+        printf("%d\n", num[i]);
+
+    return 0;
+}
