00fe13611145622961476470fd14a8fe7302bd7f
4hn6 <gb.kim@postech.ac.kr>
2020-08-08 19:53:46 +0900

    P6549 solution source code

---
+++ P06549/P6549.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+typedef long long ll;
+
+int n;
+ll h[100001];
+
+ll histogram(int s, int e)
+{
+    if(s == e) return h[s];
+    if(s+1 == e) return max(max(h[s], h[e]), 2*(min(h[s], h[e])));
+
+    int mid = (e+s)/2;
+    ll ret = max(histogram(s, mid), histogram(mid+1, e)), minv = h[mid];
+    int left = mid, right = mid, w = 1;
+
+    while(right-left < e-s)
+    {
+        int le = (s < left) ? min(minv, h[left-1]) : -1;
+        int re = (right < e) ? min(minv, h[right+1]) : -1;
+
+        if(le <= re)
+        {
+            w++;
+            right++;
+            minv = re;
+        }
+
+        else
+        {
+            w++;
+            left--;
+            minv = le;
+        }
+
+        ret = max(ret, w*minv);
+    }
+
+    return ret;
+}
+
+int main()
+{
+    while(1)
+    {
+        scanf("%d", &n);
+        if(n == 0) break;
+
+        for(int i = 1; i <= n; i++)
+            scanf("%lld", h+i);
+
+        printf("%lld\n", histogram(1, n));
+    }
+
+    return 0;
+}
