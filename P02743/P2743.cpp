dd22af9cb3d91d770c54f95593f9273690f0553c
4hn6 <gb.kim@postech.ac.kr>
2020-08-22 23:26:09 +0900

    P2743 solution source code

---
+++ P02743/P2743.cpp
@@ -0,0 +1,16 @@
+#define onlycc ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+    onlycc;
+    string str;
+    cin >> str;
+    cout << str.size();
+    return 0;
+}
