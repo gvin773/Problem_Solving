37e2687379cd8618a7149d6f06fb7a628df04a3d
4hn6 <gb.kim@postech.ac.kr>
2020-11-04 19:47:18 +0900

    P1168 solution source code

--- P01168/P1168.cpp
+++ P01168/P1168.cpp
@@ -1,34 +1,94 @@
-#define onlycc ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
+#define onlycc ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 #include <iostream>
 #include <algorithm>
+#include <string>
 #include <vector>
 using namespace std;
 
 typedef long long ll;
 
+int n, k;
+
+class Fenwick
+{
+public:
+    Fenwick(int n)
+    {
+        Tree.resize(n + 1);
+        Tree[0] = 0;
+        for (int i = 1; i <= n; i++)
+            update(i, 1);
+    }
+
+    int sum(int Index)
+    {
+        int ret = 0;
+        while (Index > 0)
+        {
+            ret += Tree[Index];
+            Index -= (Index & -Index);
+        }
+
+        return ret;
+    }
+
+    void update(int Index, int num)
+    {
+        while (Index < Tree.size())
+        {
+            Tree[Index] += num;
+            Index += (Index & -Index);
+        }
+    }
+
+    vector<int> Tree;
+};
+
+int num_person(int i, int j, Fenwick &fenwick)
+{
+    j %= n;
+    if(j == 0) j = n;
+    if(j >= i) return fenwick.sum(j) - fenwick.sum(i);
+    return fenwick.sum(n) - fenwick.sum(i) + fenwick.sum(j);
+}
+
 int main()
 {
-    vector<int> v;
-    int n, k;
+    onlycc;
     cin >> n >> k;
 
-    for(int i = 1; i <= n; i++)
-        v.push_back(i);
+    Fenwick fenwick(n);
+    cout << "<" << k;
+    fenwick.update(k, -1);
 
-    cout << '<';
-    int cur = 0, num = n;
-    for(int i = 0; i < n; i++)
+    int cur = k;
+    int left = n - 1;
+    while(left > 0)
     {
-        cur = (cur+k) % num;
-        if(cur == 0) cur = num;
-        if(i == n-1) cur = 0;
+        int L = cur + 1;
+        int R = cur + n;
+        while(L < R)
+        {
+            int mid = (L+R) / 2;
+            int val = num_person(cur, mid, fenwick);
+            int MOD = k % left;
 
-        if(i != n-1) cout << *(v.begin()+ (--cur)) << ", ";
-        else cout << *v.begin() << '>';
+            if(MOD == 0) MOD = left;
+            if(val < MOD) L = mid + 1;
+            else if(val == MOD) R = mid;
+            else R = mid-1;
+        }
 
-        v.erase(v.begin() + cur);
-        num--;
+        L %= n;
+        if(L == 0) L = n;
+        cout << ", " << L;
+
+        cur = L;
+        fenwick.update(L, -1);
+
+        left--;
     }
+    cout << ">";
 
     return 0;
 }
