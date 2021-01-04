b96eeb14ac2b0fe5d5f147b523f66807b1e5b476
4hn6 <gb.kim@postech.ac.kr>
2021-01-04 20:08:12 +0900

    P1074 solution source code

--- P01074/P1074.cpp
+++ P01074/P1074.cpp
@@ -1,54 +1,35 @@
+#define onlycc ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 #include <iostream>
 #include <algorithm>
+#include <string>
 #include <cmath>
 using namespace std;
 
 typedef long long ll;
 
-int n, r, c, num;
+ll n, r, c, num, ans;
 
-void check(int s, int x, int y)
+ll check(ll s, ll x, ll y, ll sum)
 {
-    int i, j;
+    if(s == 2) return sum + ans;
 
-    if((x+s-1) < r && (y+s-1) < c)
-    {
-        num += s*s;
-        return;
-    }
-
-    if(s == 2)
-    {
-        for(i = x; i < x+2; i++)
-        {
-            for(j = y; j < y+2; j++)
-            {
-                num++;
-                if(i == r && j == c)
-                {
-                    printf("%d", num-1);
-                    exit(0);
-                }
-            }
-        }
-
-        return;
-    }
-
-    int ns = s/2;
-
-    check(ns, x, y);
-    check(ns, x, y+ns);
-
-    check(ns, x+ns, y);
-    check(ns, x+ns, y+ns);
+    if(x <= s/2 && y <= s/2) return check(s/2, x, y, sum);
+    else if(x <= s/2 && y > s/2) return check(s/2, x, y-s/2, sum+s/2*s/2);
+    else if(x > s/2 && y <= s/2) return check(s/2, x-s/2, y, sum+s/2*s);
+    else if(x > s/2 && y > s/2) return check(s/2, x-s/2, y-s/2, sum+s/2*s/2*3);
 }
 
 int main()
 {
-    scanf("%d%d%d", &n, &r, &c);
+    onlycc;
+    cin >> n >> r >> c;
 
-    check((int)pow(2,n), 0, 0);
+    if(r%2 == 0 && c%2 == 0) ans = 1;
+    else if(r%2 == 0 && c%2 == 1) ans = 2;
+    else if(r%2 == 1 && c%2 == 0) ans = 3;
+    else if(r%2 == 1 && c%2 == 1) ans = 4;
+
+    cout << check(pow((ll)2,n), r+1, c+1, 0)-1;
 
     return 0;
 }
