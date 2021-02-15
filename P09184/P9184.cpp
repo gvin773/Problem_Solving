be75fd4a2846cb291a0fb20be72794bdf74268b8
4hn6 <gb.kim@postech.ac.kr>
2021-02-15 17:59:19 +0900

    P9184 solution source code

---
+++ P09184/P9184.cpp
@@ -0,0 +1,34 @@
+#define onlycc ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <cstring>
+using namespace std;
+
+typedef long long ll;
+
+int a, b, c, dp[21][21][21];
+
+int w(int a, int b, int c)
+{
+    if(a <= 0 || b <= 0 || c <= 0) return 1;
+    if(a > 20 || b > 20 || c > 20) return w(20, 20, 20);
+    if(dp[a][b][c] != -1) return dp[a][b][c];
+    if(a < b && b < c) return dp[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c);
+
+    return dp[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1);
+}
+
+int main()
+{
+    onlycc;
+    while(1)
+    {
+        cin >> a >> b >> c;
+        memset(dp, -1, sizeof(dp));
+        if(a == b && b == c && c == -1) break;
+        cout << "w(" << a << ", " << b << ", " << c << ") = " << w(a, b, c) << '\n';
+    }
+
+    return 0;
+}
