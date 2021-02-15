940d8c991d9b6e519c2df7e4afe86ac9ceb8f1a1
4hn6 <gb.kim@postech.ac.kr>
2021-02-16 00:25:16 +0900

    P15663 solution source code

---
+++ P15663/P15663.cpp
@@ -0,0 +1,53 @@
+#define onlycc ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+using namespace std;
+
+typedef long long ll;
+
+int n, m, num[8];
+bool visited[8];
+vector<int> v;
+set<vector<int>> s;
+
+void track()
+{
+    if(v.size() == m)
+    {
+        s.insert(v);
+        return;
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        if(!visited[i])
+        {
+            visited[i] = true;
+            v.push_back(num[i]);
+            track();
+
+            visited[i] = false;
+            v.pop_back();
+        }
+    }
+}
+
+int main()
+{
+    onlycc;
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) cin >> num[i];
+    sort(num, num+n);
+    track();
+    for(auto i = s.begin(); i != s.end(); i++)
+    {
+        for(int j : *i)
+            cout << j << ' ';
+        cout << '\n';
+    }
+
+    return 0;
+}
