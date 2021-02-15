66550337ef5122a2f37c855801c3257f58c08cba
4hn6 <gb.kim@postech.ac.kr>
2021-02-15 20:41:17 +0900

    P15652 solution source code

---
+++ P15652/P15652.cpp
@@ -0,0 +1,37 @@
+#define onlycc ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
+
+typedef long long ll;
+
+int n, m;
+vector<int> v;
+
+void track(int now)
+{
+    if(v.size() == m)
+    {
+        for(int i : v) cout << i << ' ';
+        cout << '\n';
+        return;
+    }
+    if(now > n) return;
+
+    v.push_back(now);
+    track(now);
+
+    v.pop_back();
+    track(now+1);
+}
+
+int main()
+{
+    onlycc;
+    cin >> n >> m;
+    track(1);
+
+    return 0;
+}
