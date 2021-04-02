8767e2f378c8f690bd5e01efdc74f535da0a30b9
4hn6 <gb.kim@postech.ac.kr>
2021-04-03 03:49:26 +0900

    p21293 solution source code

---
+++ P21293/P21293.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int z,y,x,w,v,u,t,s,r,p,o,n,m,l,k,j,i,h,g,f,e,d,c,b,a;
+int main()
+{
+    cin >>z>>y>>x>>w>>v>>u>>t>>s>>r
+        >>p>>o>>n>>m>>l>>k>>j>>i>>h
+        >>g>>f>>e>>d>>c>>b>>a;
+    cout<<a-b<<'\n'<<a*b<<'\n'<<a+b+1;
+}
