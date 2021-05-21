17889de7dfe95112525162160fc5c7e8ddb51deb
4hn6 <gb.kim@postech.ac.kr>
2021-05-21 14:42:15 +0900

    P1132 solution source code

---
+++ P01132/P1132.cpp
@@ -0,0 +1,68 @@
+#define onlycc ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+typedef long long ll;
+
+ll n, ans, first[30], cnt;
+string w[50];
+pair<ll, char> alpha[30];
+
+int main()
+{
+    onlycc;
+    cin >> n;
+    for(int i = 'A'; i <= 'Z'; i++)
+        alpha[i-'A'].second = i;
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> w[i];
+
+        ll m = 1;
+        for(int j = (ll)w[i].size()-1; j >= 0; j--)
+        {
+            alpha[w[i][j] - 'A'].first += m;
+            m *= 10;
+            if(j == 0) first[w[i][j] - 'A'] = true;
+        }
+    }
+
+    for(int i = 0; i < 30; i++)
+        if(alpha[i].first > 0) cnt++;
+    sort(alpha, alpha+30);
+
+    if(cnt != 10)
+    {
+        for(int i = 0; i < cnt; i++)
+            ans += (9-i)*alpha[29-i].first;
+    }
+
+    else
+    {
+        ll zero = 464646;
+        for(int i = 0; i < 30; i++)
+        {
+            if(alpha[i].first > 0 && first[alpha[i].second - 'A'] != true)
+            {
+                zero = i;
+                break;
+            }
+        }
+
+        int num = 9;
+        for(int i = 29; i > 29 - cnt; i--)
+        {
+            if(i != zero)
+            {
+                ans += num*alpha[i].first;
+                num--;
+            }
+        }
+    }
+
+    cout << ans;
+    return 0;
+}
