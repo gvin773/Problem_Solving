a290bd2747300471853d3e5b3ebcea61a306fbc7
4hn6 <gb.kim@postech.ac.kr>
2021-08-25 19:58:02 +0900

    P19532 source code

--- BOJ/P19500s/P19532/P19532.c
+++ BOJ/P19500s/P19532/P19532.c
@@ -1,69 +1,58 @@
-#include <stdio.h>
+#define onlycc ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
 
-int main(void)
+typedef long long ll;
+
+int equation_1[3], equation_2[3], new_equ[3], x, y;
+
+int main()
 {
-    int equation_1[3], equation_2[3], new_equ[3];
-    int i;
-
-    int x, y;
-    int temp;
-
-    /*input data*/
-    for (i = 0; i < 3; i++)
-        scanf("%d", &equation_1[i]);
-
-    for (i = 0; i < 3; i++)
-        scanf("%d", &equation_2[i]);
-
+    onlycc;
+    for(int i = 0; i < 3; i++) cin >> equation_1[i];
+    for(int i = 0; i < 3; i++) cin >> equation_2[i];
 
     if(equation_1[0] == 0)
     {
         y = equation_1[2]/equation_1[1];
         x = (equation_2[2]-equation_2[1]*y)/equation_2[0];
-        printf("%d %d", x, y);
-        return 0;
+        cout << x << ' ' << y;
     }
 
-    if(equation_1[1] == 0)
+    else if(equation_1[1] == 0)
     {
         x = equation_1[2]/equation_1[0];
         y = (equation_2[2]-equation_2[0]*x)/equation_2[1];
-        printf("%d %d", x, y);
-        return 0;
+        cout << x << ' ' << y;
     }
 
-    if(equation_2[0] == 0)
+    else if(equation_2[0] == 0)
     {
         y = equation_2[2]/equation_2[1];
         x = (equation_1[2]-equation_1[1]*y)/equation_1[0];
-        printf("%d %d", x, y);
-        return 0;
+        cout << x << ' ' << y;
     }
 
-    if(equation_2[1] == 0)
+    else if(equation_2[1] == 0)
     {
         x = equation_2[2]/equation_2[0];
         y = (equation_1[2]-equation_1[0]*x)/equation_1[1];
-        printf("%d %d", x, y);
-        return 0;
+        cout << x << ' ' << y;
     }
 
-    temp = equation_1[0];
+    else
+    {
+        int temp = equation_1[0];
+        for(int i = 0; i < 3; i++) equation_1[i] *= equation_2[0];
+        for(int i = 0; i < 3; i++) equation_2[i] *= temp;
+        for(int i = 0; i < 3; i++) new_equ[i] = equation_1[i] - equation_2[i];
 
-    for (i = 0; i < 3; i++)
-        equation_1[i] *= equation_2[0];
-
-    for (i = 0; i < 3; i++)
-        equation_2[i] *= temp;
-
-    for (i = 0; i < 3; i++)
-        new_equ[i] = equation_1[i] - equation_2[i];
-
-
-    y = new_equ[2] / new_equ[1];
-    x = (equation_1[2] - y * equation_1[1]) / equation_1[0];
-
-    printf("%d %d", x, y);
+        y = new_equ[2] / new_equ[1];
+        x = (equation_1[2] - y * equation_1[1]) / equation_1[0];
+        cout << x << ' ' << y;
+    }
 
     return 0;
 }
